85060f1fc67c9d1ad221d1b0e4ce6a6e7be491b2
shankhabrato501 <101513984+shankhabrato501@users.noreply.github.com>
2022-04-16 02:27:14 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -20,8 +20,8 @@ PAGE_NO = 1
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading...📤"
-    STATUS_DOWNLOADING = "Downloading...📥"
+    STATUS_UPLOADING = "Uploading...📤⏫✅ "
+    STATUS_DOWNLOADING = "Downloading...📥⏬✅ "
     STATUS_CLONING = "Cloning...♻️"
     STATUS_WAITING = "Queued...💤"
     STATUS_FAILED = "Failed 🚫. Cleaning Download..."
